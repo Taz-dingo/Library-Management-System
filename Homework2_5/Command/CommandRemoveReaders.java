9c64ecaadc8c6602801f28ac9b26cc31db03a338
塔斯丁苟 <90624111+Taaazdingo@users.noreply.github.com>
2022-05-01 11:09:13 +0800

    弃用此类，改为Library内部类

--- Homework2_5/Command/CommandRemoveReaders.java
+++ Homework2_5/Command/CommandRemoveReaders.java
@@ -1,19 +1,19 @@
-package Homework2_5.Command;
+// package Homework2_5.Command;
 
-import Homework2_5.Library;
+// import Homework2_5.Library;
 
-public class CommandRemoveReaders extends Command {
-    public CommandRemoveReaders(Library library) {super(library);}
+// public class CommandRemoveReaders extends Command {
+//     public CommandRemoveReaders(Library library) {super(library);}
 
-    @Override
-    public void doCmd(String[] words) {
-        if (words.length >= 2)
-        {
-            if(library.removeReader(words))
-                System.out.println("已成功移除！");
-            else
-                System.out.println("查无此人！");
-        }else
-            System.out.println("没听懂，请检查指令是否输入正确！");
-    }
-}
+//     @Override
+//     public void doCmd(String[] words) {
+//         if (words.length >= 2)
+//         {
+//             if(library.removeReader(words))
+//                 System.out.println("已成功移除！");
+//             else
+//                 System.out.println("查无此人！");
+//         }else
+//             System.out.println("没听懂，请检查指令是否输入正确！");
+//     }
+// }
