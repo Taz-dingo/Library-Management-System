d44a7ec458192bc17bec74f0a4771345dd1db457
塔斯丁苟 <90624111+Taaazdingo@users.noreply.github.com>
2022-05-01 11:09:32 +0800

    弃用此类，改为Library内部类

--- Homework2_5/Command/CommandReturnBook.java
+++ Homework2_5/Command/CommandReturnBook.java
@@ -1,30 +1,30 @@
-package Homework2_5.Command;
+// package Homework2_5.Command;
 
-import Homework2_5.BookRecorder;
-import Homework2_5.Library;
+// import Homework2_5.BookRecorder;
+// import Homework2_5.Library;
 
-public class CommandReturnBook extends Command{
-    public CommandReturnBook(Library library) {
-        super(library);
-    }
+// public class CommandReturnBook extends Command{
+//     public CommandReturnBook(Library library) {
+//         super(library);
+//     }
 
-    @Override
-    public void doCmd(String[] words) {
-        if(words.length>=2) {
-            BookRecorder recorder = library.searchBookIndex(words);
-            if(words.length>=2) {
-                if (recorder != null) {
-                    recorder.returnBook(words);
-                    System.out.println("借书读者：" + recorder.getReader().getName() + " 借书时间：" + recorder.getLendDate());
-                    System.out.println("还书成功！还书时间：" + recorder.getReturnDate());
-                }
-                else{
-                    System.out.println("馆内没有这个编号的书！");
-                }
-            }
-        }
-        else {
-            System.out.println("没听懂，请检查指令是否输入正确！");
-        }
-    }
-}
+//     @Override
+//     public void doCmd(String[] words) {
+//         if(words.length>=2) {
+//             BookRecorder recorder = library.searchBookIndex(words);
+//             if(words.length>=2) {
+//                 if (recorder != null) {
+//                     recorder.returnBook(words);
+//                     System.out.println("借书读者：" + recorder.getReader().getName() + " 借书时间：" + recorder.getLendDate());
+//                     System.out.println("还书成功！还书时间：" + recorder.getReturnDate());
+//                 }
+//                 else{
+//                     System.out.println("馆内没有这个编号的书！");
+//                 }
+//             }
+//         }
+//         else {
+//             System.out.println("没听懂，请检查指令是否输入正确！");
+//         }
+//     }
+// }
